d658d55b8ce57db231939049d87dda90bbbc7447
Becky Zorn <bczorn@gmail.com>
2019-08-22 03:49:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  batch_array = Array.new
+  array.each do |x|
+    i = badge_maker(x)
+    batch_array.push(i)
+  end
+  return batch_array
+end
+
+def assign_rooms(array)
+  room_array = Array.new
+  array.each do |x|
+    room = array.index(x) + 1
+    room_array.push("Hello, #{x}! You'll be assigned to room #{room}!")
+  end
+  return room_array
+end
+
+def printer(attendees)
+ attendees.each do |x|
+  puts badge_maker(x)
+  puts assign_rooms(x)
+  end
+end
